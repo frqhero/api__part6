8c6ab5ce45afe7ad271ab441d37d7c7fe6b839af
frqhero <frqhero@gmail.com>
2023-05-09 18:13:16 +0600

    Initial commit

---
+++ main.py
@@ -0,0 +1,86 @@
+import os
+
+from dotenv import load_dotenv
+import requests
+
+
+def download_image(url, abs_path, params=None):
+    response = requests.get(url, params=params)
+    response.raise_for_status()
+    with open(abs_path, 'wb') as file:
+        file.write(response.content)
+
+
+def download_python_comics():
+    link = 'https://xkcd.com/353/info.0.json'
+    response = requests.get(link)
+    response.raise_for_status()
+    deserialized_response = response.json()
+    file_link = deserialized_response['img']
+    file_name = file_link.split('/')[-1]
+    alt = deserialized_response['alt']
+    download_image(file_link, file_name)
+    return file_name, alt
+
+
+def get_address_to_upload(token):
+    link = 'https://api.vk.com/method/photos.getWallUploadServer'
+    params = {
+        'access_token': token,
+        'v': '5.124'
+    }
+    response = requests.get(link, params=params)
+    response.raise_for_status()
+    return response.json()['response']['upload_url']
+
+
+def upload_picture(address, file_name):
+    with open(file_name, 'rb') as file:
+        url = address
+        files = {
+            'photo': file,
+        }
+        response = requests.post(url, files=files)
+        response.raise_for_status()
+    return response.json()
+
+
+def save_wall_photo(uploaded_photo_data):
+    link = 'https://api.vk.com/method/photos.saveWallPhoto'
+    response = requests.post(link, params=uploaded_photo_data)
+    response.raise_for_status()
+    response_deserialized = response.json()['response'][0]
+    return (
+        f"photo{response_deserialized['owner_id']}_"
+        f"{response_deserialized['id']}"
+    )
+
+
+def wall_post(saved_photo_data):
+    link = 'https://api.vk.com/method/wall.post'
+    response = requests.post(link, params=saved_photo_data)
+    response.raise_for_status()
+    1
+
+def main():
+    file_name, alt = download_python_comics()
+    load_dotenv()
+    token = os.getenv('VK_TOKEN')
+    address_to_upload = get_address_to_upload(token)
+    uploaded_photo_data = upload_picture(address_to_upload, file_name)
+    uploaded_photo_data['access_token'] = token
+    uploaded_photo_data['v'] = '5.124'
+    saved_photo = save_wall_photo(uploaded_photo_data)
+    saved_photo_data = {
+        'owner_id': '-220382476',
+        'attachments': saved_photo,
+        'message': alt,
+        'from_group': '1',
+        'access_token': token,
+        'v': '5.124'
+    }
+    wall_post(saved_photo_data)
+
+
+if __name__ == '__main__':
+    main()
